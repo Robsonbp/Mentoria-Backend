48d6b38315abebdb8c1f1969408429910c85c0b3
Robson Pinto <robsonbpinto@outlook.com>
2024-03-15 20:52:57 -0300

    Renomeado o arquivo pessoas.py, antes era lista.py

--- routes/pessoas.py
+++ routes/pessoas.py
@@ -8,14 +8,24 @@ from database.config.conexao import conexao_db
 from schemas import pessoas
 
 
-router = APIRouter(prefix="/lista")
+router = APIRouter(prefix="/pessoas")
 
 @router.get("/")
-def get_lista():
+def get_pessoas():
     try:
         conexao = conexao_db(config("DATABASE"))
         pessoas_list: pessoas.PessoaList = pessoas.get_pessoas(conexao=conexao)
         return pessoas_list
+    except Exception as err:
+        print(err)
+        return HTTPException(status_code=400, detail=str(err))
+
+@router.get("/{id_pessoa}")
+def get_pessoa(id_pessoa: int):
+    try:
+        conexao = conexao_db(config("DATABASE"))
+        pessoa: pessoas.Pessoa = pessoas.get_pessoa(conexao=conexao, id=id_pessoa)
+        return pessoa
     except Exception as err:
         print(err)
         return HTTPException(status_code=400, detail=str(err))
