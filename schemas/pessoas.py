3218140e538a2957f5f286f4d0badfe87fe2973d
Robson Pinto <robsonbpinto@outlook.com>
2024-03-15 20:55:48 -0300

    Adicionado o id ao schema Pessoa e querts adaptadas para o postgresql

--- schemas/pessoas.py
+++ schemas/pessoas.py
@@ -1,9 +1,11 @@
 from pydantic import BaseModel
 from datetime import datetime, date
 from pyodbc import Connection
+from typing import Optional
 
 
 class Pessoa(BaseModel):
+    id: Optional[int] = 0
     nome: str
     email: str
     dt_nascimento: date
@@ -11,29 +13,50 @@ class Pessoa(BaseModel):
     def cadastrar_pessoa(self, conexao: Connection):
         cursor = conexao.cursor()
         cursor.execute(f"""
-            insert into pessoas (nome, email, dt_nascimento)
+            insert into public."Pessoas" (nome, email, dt_nascimento)
             values
             ({self.nome}, {self.email}, {self.dt_nascimento})
         """)
 
 
 class PessoaList(BaseModel):
-    lista: list[Pessoa]
+    pessoas: list[Pessoa]
 
 
 def get_pessoas(conexao: Connection) -> PessoaList:
     cursor = conexao.cursor()
     cursor.execute(f"""
-        select * from pessoas
+        select * from public."Pessoas"
     """)
     tabela_sql = cursor.fetchall()
-    pessoas: list = []
+    pessoa_ls: list = []
     for registro in tabela_sql:
-        pessoas.append(
+        pessoa_ls.append(
             Pessoa(
+                id=registro.id,
                 nome=registro.nome,
                 email=registro.email,
                 dt_nascimento=registro.dt_nascimento
             )
         )
-    return PessoaList(lista=pessoas)
+    return PessoaList(pessoas=pessoa_ls)
+
+
+def get_pessoa(conexao: Connection, id: int) -> Pessoa:
+    cursor = conexao.cursor()
+    cursor.execute(f"""
+        select * from pessoas
+        where id = {id}
+    """)
+    tabela_sql = cursor.fetchall()
+    pessoa_ls: list = []
+    for registro in tabela_sql:
+        pessoa_ls.append(
+            Pessoa(
+                id=registro.id,
+                nome=registro.nome,
+                email=registro.email,
+                dt_nascimento=registro.dt_nascimento
+            )
+        )
+    return PessoaList(pessoas=pessoa_ls)
